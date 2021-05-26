d14aa1f2098a9888a732b284bbb420050be1b26c
ngahthi <nthi@vcu.edu>
2021-05-26 18:02:54 -0400

    Create Hello.java
    Lab 01

---
+++ Lab 1/Hello.java
@@ -0,0 +1,21 @@
+/*************************************
+* Java Program Name
+**************************************
+* Nga Thi
+* 05/26/2021
+* CMSC 255 C01
+**************************************/
+
+public class Hello {
+	public static void main (String[] args){
+
+	System.out.println("Nga Thi");
+	System.out.println("Lab 01");
+	System.out.println("Java Test");
+	System.out.println("05/26/2021");
+	System.out.println("CMSC 255 C01");
+	System.out.println("");
+
+
+	}
+}
